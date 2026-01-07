a67ca351e1fff7a8e71c5be1acb827a260a380c4
erosalesPRY <ejra2008@gmail.com>
2026-01-07 15:53:37 -0500

    Add files via upload

--- HelpDesk/Incidencia/DetalleIncidnecia.aspx.cs
+++ HelpDesk/Incidencia/DetalleIncidnecia.aspx.cs
@@ -16,7 +16,7 @@ using static EasyControlWeb.InterConeccion.EasyDataInterConect;
 
 namespace SIMANET_W22R.HelpDesk.Incidencia
 {
-    public partial class DetalleIncidnecia : HelpDeskBase, IPaginaBase
+    public partial class DetalleIncidnecia : HelpDeskBase,IPaginaBase
     {
 
         protected void Page_Load(object sender, EventArgs e)
@@ -123,16 +123,6 @@ namespace SIMANET_W22R.HelpDesk.Incidencia
             }
             
 
-        }
-
-        public void CargarModoNuevo()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void CargarModoModificar()
-        {
-            throw new NotImplementedException();
         }
 
         public void LlenarGrilla()
