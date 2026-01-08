370218534df12f40dccdd4e6ca8e73c79472a443
erosalesPRY <ejra2008@gmail.com>
2026-01-08 08:04:04 -0500

    Add files via upload

---
+++ Recursos/LibSIMA/ChatBotServiceBroker/ITILService/ITILService.js
@@ -0,0 +1,221 @@
+﻿var objInfoBE = ScriptManager.Find("ITILService");
+
+//objInfoBE.Params.GetParamValue("LoadData")
+
+//Se ejecutara al momento de enviar el mensaje
+NetSuite.LiveChat.SendMsgDelegate = function (IdMsg) {}
+
+
+function CargarRequerimientosConMSG(IdContacto) {
+    var oEasyDataInterConect = new EasyDataInterConect();
+    oEasyDataInterConect.MetododeConexion = ModoInterConect.WebServiceExterno;
+    oEasyDataInterConect.UrlWebService = ConnectService.PathNetCore + "HelpDesk/AdministrarHD.asmx";
+    oEasyDataInterConect.Metodo = "RequerimientosInMSG_lst";
+
+    var oParamCollections = new SIMA.ParamCollections();
+    var oParam = new SIMA.Param("IdContacto", IdContacto, TipodeDato.Int);
+    oParamCollections.Add(oParam);
+
+
+    oParam = new SIMA.Param("UserName", UsuarioBE.UserName);
+    oParamCollections.Add(oParam);
+
+    oEasyDataInterConect.ParamsCollection = oParamCollections;
+
+    var oEasyDataResult = new EasyDataResult(oEasyDataInterConect);
+    return oEasyDataResult.getDataTable();
+}
+
+
+function ObtenerDatosdeGrupo(IdGrupo) {
+    var oEasyDataInterConect = new EasyDataInterConect();
+    oEasyDataInterConect.MetododeConexion = ModoInterConect.WebServiceExterno;
+    oEasyDataInterConect.UrlWebService = EasyNetLiveChat.PathUrlServiceChat;
+    oEasyDataInterConect.Metodo = "DetalleContactoXID";
+
+    var oParamCollections = new SIMA.ParamCollections();
+    var oParam = new SIMA.Param("Id", IdGrupo, TipodeDato.Int);
+    oParamCollections.Add(oParam);
+    oParam = new SIMA.Param("UserName", UsuarioBE.UserName);
+    oParamCollections.Add(oParam);
+
+    oEasyDataInterConect.ParamsCollection = oParamCollections;
+
+    var oEasyDataResult = new EasyDataResult(oEasyDataInterConect);
+    return oEasyDataResult.getEntity();
+}
+function GrabaRquqerimientodeIncidencia() {
+    //DetalleIncidnecia
+
+    var oParamCollections = new SIMA.ParamCollections();
+    var oParam = new SIMA.Param("Descripcion", EasyTxtDescripcion.GetValue());
+    oParamCollections.Add(oParam);
+    oParam = new SIMA.Param("IdServicioArea", DetalleIncidnecia.Params[DetalleIncidnecia.KEYIDSERVICIOAREA]);
+    oParamCollections.Add(oParam);
+    oParam = new SIMA.Param("CodigoPersonal", UsuarioBE.CodPersonal);
+    oParamCollections.Add(oParam);
+    oParam = new SIMA.Param("IdUsuario", UsuarioBE.IdUsuario, TipodeDato.Int);
+    oParamCollections.Add(oParam);
+    oParam = new SIMA.Param("UserName", UsuarioBE.UserName);
+    oParamCollections.Add(oParam);
+
+
+    var oEasyDataInterConect = new EasyDataInterConect();
+    oEasyDataInterConect.MetododeConexion = ModoInterConect.WebServiceInterno;
+    oEasyDataInterConect.UrlWebService = "/HelpDesk/Procesar.asmx"
+    oEasyDataInterConect.Metodo = "GuardarRequerimiento";
+    oEasyDataInterConect.ParamsCollection = oParamCollections;
+
+    var oEasyDataResult = new EasyDataResult(oEasyDataInterConect);
+    return oEasyDataResult.sendData().toString();
+}
+
+
+NetSuite.Manager.Broker.Persiana.Popup.Aceptar = function () {
+
+    
+
+    var IdRqr = GrabaRquqerimientodeIncidencia();
+
+    //Carga informacion del grup que brinda servcio de atencion de incidnecias
+    var CBContactoGrupoBE = ObtenerDatosdeGrupo(DetalleIncidnecia.Params[DetalleIncidnecia.KEYIDCONTACTO]);
+
+
+    oContactoDestinoBE = new NetSuite.LiveChat.ContactBE();
+    oContactoDestinoBE.IdContacto = CBContactoGrupoBE.IdContacto;
+    oContactoDestinoBE.Foto = CBContactoGrupoBE.FotoGrupo;
+    oContactoDestinoBE.Nombre = CBContactoGrupoBE.NombreGrupo; 
+    oContactoDestinoBE.Tipo = 1//Incidencia
+
+    //Display informacion de chat con el contacto destino
+    EasyAcFindContacto.SetValue(oContactoDestinoBE.IdContacto, oContactoDestinoBE.Nombre);
+
+    EasyNetLiveChat.DisplaySelected.ContactoInfo(oContactoDestinoBE);
+
+    var oMensajeBE = new NetSuite.LiveChat.MensajeBE();
+
+    var oContactFromBE = new NetSuite.LiveChat.ContactBE();
+    oContactFromBE.IdContacto = UsuarioBE.IdContacto;
+    oContactFromBE.IdMiembro = UsuarioBE.IdContacto;
+    oContactFromBE.Foto = EasyNetLiveChat.FotoContacto(UsuarioBE.NroDocumento);
+    oContactFromBE.Nombre = UsuarioBE.ApellidosyNombres;
+    
+
+    //oContactFromBE.IdMsg = pos;//Este valor debe de ser obtenido desde la BD al crear el regisro de mesajes
+    //Eatblece parametros de envio
+    oMensajeBE.ContactoFrom = oContactFromBE;
+
+    oMensajeBE.ContactoTo = oContactoDestinoBE;
+
+    var MensajeContenidoBE = new NetSuite.LiveChat.MensajeContenidoBE();
+    MensajeContenidoBE.IdMsg = oMensajeBE.IdMsg;
+    MensajeContenidoBE.IdContenido = 0;
+    MensajeContenidoBE.Texto = "Por Requerimiento";//NetSuite.LiveChat.LinkService.TemplateBubble(null);;
+    MensajeContenidoBE.AllLikes = null;
+    var CollectionMsgContenido = new Array();
+    CollectionMsgContenido.Add(MensajeContenidoBE);
+    oMensajeBE.AllContenidoBE = CollectionMsgContenido;
+    //Almacena en la BD
+    oMensajeBE.IdTablaInfo = "31";//Tabla de requerimientos
+    oMensajeBE.IdInfo = IdRqr;//Vincuo con el requerimiento generado
+
+    var jSonBE = EasyNetLiveChat.Data.GuardarMensaje(oMensajeBE);
+    oMensajeBE.IdMsg = jSonBE.OutIdMsg;
+    MensajeContenidoBE.IdMsg = oMensajeBE.IdMsg;
+    MensajeContenidoBE.IdContenido = jSonBE.OutIdContenido;
+
+
+    NetSuite.LiveChat.EnviaMensaje(oMensajeBE);
+
+
+    //Carga los eventros del contacto
+    NetSuite.Manager.Broker.LoadEvent(DetalleIncidnecia.Params[DetalleIncidnecia.KEYIDCONTACTO]);
+
+
+
+    return true;
+}
+
+//Invocado Internamente para ser acoplado al mensaje relacionado con dicha informaciom
+NetSuite.Manager.Broker.onItemMsgBubble = function (oDataInfo) {
+    //Aqui buscar en el datatable previamente cargada de requerimientos relacionado con los mensajes
+    var strHtml = "";
+    
+    if (DataTableRqr != null) {
+        DataTableRqr.Select("ID_MSG", "=", oDataInfo.ID_MSG).forEach(function (oDataRow, i) {
+
+            var ArrayPath = oDataRow.PATHSERVICE.toString().split('|');
+            var htmlItem = '';
+            ArrayPath.reverse().forEach(function (ItemPathText, p) {
+                htmlItem += '        <div class="btn btn-default">' + ItemPathText + '</div>';
+            });
+
+            var iTemplatePath = '<div class="container ScrollPath" style="width: px;">'
+                + '<div class="row">'
+                + '    <div id="EasyPathService_BarPath" class="btn-group btn-breadcrumb">'
+                + '        <div class="btn btn-primary">'
+                + '            <div class="fa fa-home"></div>'
+                + '        </div>'
+                + htmlItem
+                + '    </div>'
+                + '</div>'
+                + '</div>';
+
+            var iTemplateProgress = '<div class="progress">'
+                + '  <div class="progress-bar"  style = "width: ' + oDataRow["AVANCE"] + '%; ">'
+                + '      <span class="progress-bar-text">' + oDataRow["AVANCE"] + '%</span>'
+                + '  </div>'
+                + '</div>';
+
+
+            strHtml = '<table>'
+                + '  <tr>'
+                + '     <td>'
+                + iTemplatePath
+                + '     </td>'
+                + '   </tr>'
+                + '   <tr>'
+                + '     <td>'
+                + oDataRow["TEXT"]
+                + '     </td>'
+                + '  </tr>'
+                + '  <tr>'
+                + '     <td>'
+                + iTemplateProgress
+                + '     </td>'
+                + '  </tr>'
+                + '</table>';
+
+        });
+    }
+    else {
+        strHtml = 'Sin Exito';
+    }
+    return strHtml;
+}
+
+//Evento implementado y llamado al momento de dat click a la burbuja de mensajes
+NetSuite.LiveChat.bubble.OnClick = function (oMgsItem) {
+    alert(oMgsItem);
+}
+
+
+
+
+var DataTableRqr = null;
+//Metodo sobreescrito que particulariza la informacion segun la linreria relacionada al grupoi
+NetSuite.Manager.Broker.LoadEvent = function (idGrupoContacto) {
+        try {
+            if (idGrupoContacto != undefined) {
+                DataTableRqr = CargarRequerimientosConMSG(idGrupoContacto);
+            }
+            else {
+                DataTableRqr =  (EasyAcFindContacto.GetValue());
+            }
+        }
+        catch (ex) {
+            
+            DataTableRqr = CargarRequerimientosConMSG(EasyAcFindContacto.GetValue());
+        }
+}
+//alert('Registrado');
---
+++ Recursos/LibSIMA/ChatBotServiceBroker/ITILService/OpcionesdeServicios.aspx
@@ -0,0 +1,162 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="OpcionesdeServicios.aspx.cs" Inherits="SIMANET_W22R.Recursos.LibSIMA.ChatBotServiceBroker.ITILService.OpcionesdeServicios" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+  
+    <style>
+        
+.chat-container {
+  width: 300px;
+  background: #fff;
+  border-radius: 8px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  padding: 10px;
+}
+
+#chatbox {
+  height: 300px;
+  overflow-y: auto;
+  border: 1px solid #ddd;
+  padding: 10px;
+  margin-bottom: 10px;
+  border-radius: 5px;
+}
+
+.bot-message, .user-message {
+  margin: 5px 0;
+  padding: 8px 10px;
+  border-radius: 5px;
+}
+
+.bot-message {
+  background-color: #e0f7fa;
+  align-self: flex-start;
+}
+
+.user-message {
+  background-color: #c8e6c9;
+  align-self: flex-end;
+}
+
+#userInput {
+  width: calc(100% - 60px);
+  padding: 8px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+
+#sendBtn {
+  width: 50px;
+  padding: 8px;
+  background-color: #00796b;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+#sendBtn:hover {
+  background-color: #004d40;
+}
+    </style>
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        <button id="write" type="button">Write!</button>
+        <h1 id="text"></h1>
+
+
+
+         <div class="chat-container">
+            <div id="chatbox">
+              <div class="bot-message">¡Hola! ¿En qué puedo ayudarte?</div>
+            </div>
+            <input type="text" id="userInput" placeholder="Escribe tu mensaje aquí..." />
+            <button  type="button" id="sendBtn">Enviar</button>
+          </div>
+
+
+
+    </form>
+
+    <script>
+        
+      
+
+
+        function escribir(contenedor, writer, speed) {
+
+            longitud = writer.length;
+
+            cnt = document.getElementById(contenedor);
+            var i = 0;
+            tiempo = setInterval(function () {
+                cnt.innerHTML = cnt.innerHTML.substr(0, cnt.innerHTML.length - 1) + writer.charAt(i) + " ";
+                if (i >= longitud) {
+                    clearInterval(tiempo);
+                    cnt.innerHTML = cnt.innerHTML.substr(0, longitud);
+                    return true;
+                } else {
+                    i++;
+                }
+            }, speed);
+        };
+
+
+       
+
+        var texto = "Llamada oculta. Al otro lado del teléfono suena una voz muy grave, fuerte, de un hombre con un inglés de ligero acento escandinavo. Soy Lars Hedegaard, creo que querías hablar conmigo. Verse no es posible. Ni se encuentra en Copenhague ni puede dar su paradero al estar bajo protección policial. Hedegaard, historiador y periodista danés de 74 años, es un reconocido y duro crítico del islam. Le grabaron en su casa, sin previo aviso según defiende, diciendo cosas como que en las familias musulmanas.";
+        $("#write").click(function () {
+            escribir("text", texto, 100);
+        });
+
+
+    </script>
+
+
+    <script>
+        chatbox = document.getElementById("chatbox");
+        userInput = document.getElementById("userInput");
+        sendBtn = document.getElementById("sendBtn");
+
+        // Respuestas aleatorias del chatbot
+        const responses = [
+            "¡Qué interesante!",
+            "No estoy seguro, pero puedo investigar.",
+            "¿Puedes explicarlo un poco más?",
+            "¡Eso suena genial!",
+            "Déjame pensarlo un momento...",
+            "¡Claro que sí!",
+            "No entiendo del todo, ¿puedes repetirlo?"
+        ];
+
+        // Función para agregar mensajes al chat
+        function addMessage(message, sender) {
+            messageDiv = document.createElement("div");
+            messageDiv.classList.add(sender === "bot" ? "bot-message" : "user-message");
+            messageDiv.textContent = message;
+            chatbox.appendChild(messageDiv);
+            chatbox.scrollTop = chatbox.scrollHeight; // Desplazar hacia abajo
+        }
+
+        // Enviar mensaje del usuario y respuesta del bot
+        sendBtn.addEventListener("click", () => {
+            const userMessage = userInput.value.trim();
+            if (userMessage) {
+                addMessage(userMessage, "user");
+                userInput.value = "";
+
+                // Respuesta aleatoria del bot
+                const botResponse = responses[Math.floor(Math.random() * responses.length)];
+                setTimeout(() => addMessage(botResponse, "bot"), 900);
+            }
+        });
+    </script>
+
+</body>
+</html>
