d2dbc6c1bf91420ad3f6982d9384e37d99745ee2
sima-otic-vyr <vybanez@SPEDVYBANEZ>
2026-04-15 14:35:40 -0500

    validaciones para el reporte: 3. Detalle - Analisis de Gastos Directos de OT por Items de Asiento

--- GestionCostos/Pagos/Pagos.asmx.cs
+++ GestionCostos/Pagos/Pagos.asmx.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using SIMANET_W22R.srvGestionCostos;
+using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
-using SIMANET_W22R.srvGestionCostos;
 
 namespace SIMANET_W22R.GestionCostos.Pagos
 {
@@ -32,10 +33,95 @@ namespace SIMANET_W22R.GestionCostos.Pagos
         [WebMethod]
         public DataTable Listar_analisis_gast_itemsasient(string V_CENTRO_OPERATIVO, string V_DIVISION, string V_NUMERO_OT, string UserName)
         {
-            CostosSoapClient oP = new CostosSoapClient();
-            dt = oP.Listar_analisis_gast_itemsasient(V_CENTRO_OPERATIVO, V_DIVISION, V_NUMERO_OT, UserName);
-            dt.TableName = "SP_Analisis_Gast_itemsAsientOT";
-            return dt;
+            CostosSoapClient oCs = new CostosSoapClient();
+            DataTable dtError = new DataTable("SP_Analisis_Gast_itemsAsientOT");
+            
+            // Configura estructura tabla de error // Los campos se toman de las etiquetas de reporte crystal
+            dtError.TableName = "SP_Analisis_Gast_itemsAsientOT";
+            dtError.Columns.Add("CENTRO_COSTO", typeof(string));
+            dtError.Columns.Add("CENTRO_OPERATIVO", typeof(string));
+            try
+            {
+                // -----validamos datos Obligatorios ----
+                if (V_CENTRO_OPERATIVO == "-1" || V_CENTRO_OPERATIVO == "")
+                {
+                    DataRow row = dtError.NewRow();
+                    row["CENTRO_OPERATIVO"] = "Seleccione el Centro Operativo, es un parámetro obligatorio para retornar información";
+                    dtError.Rows.Add(row);
+                    return dtError;
+                }
+                if (V_NUMERO_OT == "0" || V_NUMERO_OT == "")
+                {
+                    DataRow row = dtError.NewRow();
+                    row["CENTRO_OPERATIVO"] = "Ingrese el Número de la OT, es un parámetro obligatorio para retornar información";
+                    dtError.Rows.Add(row);
+                    return dtError;
+                }
+                if (V_DIVISION == "-1" || V_DIVISION == "")
+                {
+                    DataRow row = dtError.NewRow();
+                    row["CENTRO_OPERATIVO"] = "Seleccione la Linea de Negocio, es un parámetro obligatorio para retornar información";
+                    dtError.Rows.Add(row);
+                    return dtError;
+                }
+               
+                // ----------------------------------------------------
+
+
+                dt = oCs.Listar_analisis_gast_itemsasient(V_CENTRO_OPERATIVO, V_DIVISION, V_NUMERO_OT, UserName);
+                dt.TableName = "SP_Analisis_Gast_itemsAsientOT";
+                if (dt != null)  // valida vacio
+                {
+
+                    if (dt.Rows.Count > 0)
+                    {
+                        dt.TableName = "SP_Analisis_Gast_itemsAsientOT";
+                        return dt;
+                    }
+                    else
+                    {
+                        DataRow row = dtError.NewRow();
+                        row["CENTRO_OPERATIVO"] = "No existen registros para los parámetros consultados: " + V_CENTRO_OPERATIVO + " " + V_DIVISION +  " " + V_NUMERO_OT;
+                        dtError.Rows.Add(row);
+                        return dtError;
+                    }
+                }
+                else
+                {
+                    DataRow row = dtError.NewRow();
+                    row["CENTRO_OPERATIVO"] = "No existen registros para los parámetros consultados: " + V_CENTRO_OPERATIVO + " " + V_DIVISION +   " " + V_NUMERO_OT;
+                    dtError.Rows.Add(row);
+                    return dtError;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log del error y lanzar una excepción HTTP 500
+                DataRow row = dtError.NewRow();
+                row["CENTRO_OPERATIVO"] = "Error en servicio: " + ex.Message;
+                dtError.Rows.Add(row);
+                return dtError;
+            }
+            // evita que el servicio se bloquee por caida provocada por ese metodo
+            finally
+            {
+                if (oCs != null)
+                {
+                    try
+                    {
+                        if (oCs.State != System.ServiceModel.CommunicationState.Faulted)
+                            oCs.Close();
+                        else
+                            oCs.Abort();
+                    }
+                    catch
+                    { oCs.Abort(); }
+                }
+            }
+
         }
+
+
+
     }
 }
